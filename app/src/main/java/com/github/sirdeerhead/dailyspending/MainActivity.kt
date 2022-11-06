92a0de0babde0f9463e49f8dd23ddd9db971db82
SirDeerHead <pawlikowski.daniel@hotmail.com>
2022-11-07 00:17:01 +0100

    🎉 Initial commit

---
+++ app/src/main/java/com/github/sirdeerhead/dailyspending/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.github.sirdeerhead.dailyspending
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Bottom Tab Bar Example Fragments Android Studio Kotlin Tutorial
+// TODO: https://www.youtube.com/watch?v=obYxPd2ot7Q
+// Bottom Sheet Dialog Android Studio Kotlin Example Tutorial - ViewModel
+// TODO: https://www.youtube.com/watch?v=RzjCMa4GBD4
+// Room Database Android Studio Kotlin Example Tutorial
+// TODO: https://www.youtube.com/watch?v=-LNg-K7SncM
+// Schedule Local Notifications Android Studio Kotlin Tutorial
+// TODO: https://www.youtube.com/watch?v=_Z2S63O-1HE
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
