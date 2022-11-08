beda5e398bf3ecef40d801b998b0c57407d32b59
sirdeerhead <pawlikowski.daniel@hotmail.com>
2022-11-08 08:21:06 +0100

    Added Binding and updated androidx.core:core-ktx: in build.gradle, Added Binding in MainActivity.kt.

--- app/src/main/java/com/github/sirdeerhead/dailyspending/MainActivity.kt
+++ app/src/main/java/com/github/sirdeerhead/dailyspending/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.sirdeerhead.dailyspending
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.github.sirdeerhead.dailyspending.databinding.ActivityMainBinding
 
 // Bottom Tab Bar Example Fragments Android Studio Kotlin Tutorial
 // TODO: https://www.youtube.com/watch?v=obYxPd2ot7Q
@@ -14,8 +16,15 @@ import android.os.Bundle
 
 
 class MainActivity : AppCompatActivity() {
+
+    //Adding binding class
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Initializing binding view
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
