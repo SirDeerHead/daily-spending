0cb9cf3139b0dfcc250dd977776b483ef8191ccb
SirDeerHead <pawlikowski.daniel@hotmail.com>
2022-11-14 20:37:35 +0100

    Added colors in colors.xml, Added string in strings.xml Changed UI look for fragment_new_cash_flow.xml, Created DataPicker in NewCashFlow.kt, Deleted Unused import directive in NewCashFlow.kt, Deleted ? in View in NewCashFlow.kt.

--- app/src/main/java/com/github/sirdeerhead/dailyspending/nav/home/NewCashFlow.kt
+++ app/src/main/java/com/github/sirdeerhead/dailyspending/nav/home/NewCashFlow.kt
@@ -1,15 +1,15 @@
 package com.github.sirdeerhead.dailyspending.nav.home
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.github.sirdeerhead.dailyspending.R
 import com.github.sirdeerhead.dailyspending.databinding.FragmentNewCashFlowBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.util.*
 
 class NewCashFlow : BottomSheetDialogFragment() {
 
@@ -19,12 +19,16 @@ class NewCashFlow : BottomSheetDialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentNewCashFlowBinding.inflate(inflater, container, false)
 
         val category = resources.getStringArray(R.array.category)
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_category_items, category)
         binding.actvDropdownCategory.setAdapter(arrayAdapter)
+
+        binding.date.setOnClickListener{
+            cashFlowDatePicker()
+        }
 
         return binding.root
     }
@@ -32,5 +36,21 @@ class NewCashFlow : BottomSheetDialogFragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun cashFlowDatePicker(){
+        val cashFlowCalendar = Calendar.getInstance()
+        val year = cashFlowCalendar.get(Calendar.YEAR)
+        val month = cashFlowCalendar.get(Calendar.MONTH)
+        val day = cashFlowCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(requireContext(),
+            { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                binding.date.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        ).show()
     }
 }
