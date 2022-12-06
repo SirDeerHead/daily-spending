258320ea056a52a862b8784c6b4c4c53ed09a37e
SirDeerHead <pawlikowski.daniel@hotmail.com>
2022-12-06 21:10:02 +0100

    Added pre-populate method for testing.

--- app/src/main/java/com/github/sirdeerhead/dailyspending/nav/Stats.kt
+++ app/src/main/java/com/github/sirdeerhead/dailyspending/nav/Stats.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.github.sirdeerhead.dailyspending.databinding.FragmentStatsBinding
+import com.github.sirdeerhead.dailyspending.room.CashFlowApp
+import com.github.sirdeerhead.dailyspending.room.CashFlowDao
+import com.github.sirdeerhead.dailyspending.room.CashFlowEntity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class Stats : Fragment() {
@@ -14,14 +20,61 @@ class Stats : Fragment() {
     private var _binding: FragmentStatsBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentStatsBinding.inflate(inflater, container, false)
 
+        val cashFlowDao = (activity?.application as CashFlowApp).database.cashFlowDao()
+
+        /*
+        binding.floatingActionButton.setOnClickListener{
+            addItem(cashFlowDao)
+        }
+        */
+
         return binding.root
     }
+
+    private fun addItem(cashFlowDao: CashFlowDao) {
+        lifecycleScope.launch {
+            cashFlowDao.insert(
+                CashFlowEntity(
+                    date = "12.12.2022", amount = 20.1,
+                    category = "Jedzenie", description = "Record 1/3"
+                )
+            )
+            cashFlowDao.insert(
+                CashFlowEntity(
+                    date = "12.12.2022", amount = -20.1,
+                    category = "Jedzenie", description = "Record 2/3"
+                )
+            )
+            cashFlowDao.insert(
+                CashFlowEntity(
+                    date = "12.12.2022", amount = 0.0,
+                    category = "Jedzenie", description = "Record 3/3"
+                )
+            )
+            cashFlowDao.insert(
+                CashFlowEntity(
+                    date = "03.11.2020", amount = 123.0,
+                    category = "Ubrania", description = "Record -1/2"
+                )
+            )
+            cashFlowDao.insert(
+                CashFlowEntity(
+                    date = "04.10.2021", amount = -9990.0,
+                    category = "Relaks", description = "Record -2/2"
+                )
+            )
+        }
+
+        Toast.makeText(activity, "Cash-s Flow added", Toast.LENGTH_LONG).show()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
